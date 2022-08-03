aa3a3e03e711f03d02d99fb22595a8d15b2afbee
azhong <zczwzw@163.com>
2022-08-03 23:37:48 +0800

    chapter2 property

---
+++ chapter2property/myclass.h
@@ -0,0 +1,31 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+#include <QObject>
+
+class MyClass : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(Priority priority READ priority WRITE setPriority NOTIFY priorityChanged)
+    Q_CLASSINFO("auther", "Azhong")
+    Q_CLASSINFO("version", "3.0.0")
+
+public:
+//    explicit MyClass(QObject *parent = nullptr);
+    enum Priority{High, Low, VeryHigh, VeryLow};
+    Q_ENUM(Priority)
+
+    void setPriority(Priority priority){
+        m_priority = priority;
+        emit priorityChanged(priority);
+    }
+    Priority priority() const {return m_priority;}
+
+signals:
+    void priorityChanged(MyClass::Priority);
+
+private:
+    Priority m_priority;
+};
+
+#endif // MYCLASS_H
