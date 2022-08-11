0262308fa9673d0aec9679e86f82efe67096ce09
azhong <zczwzw@163.com>
2022-08-11 22:45:04 +0800

    frameless

--- chapter7-frameless/widget.cpp
+++ chapter7-frameless/widget.cpp
@@ -6,10 +6,19 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    setWindowFlag(Qt::FramelessWindowHint); // 设置窗口无边框，设置后窗口不能移动
+    setAttribute(Qt::WA_TranslucentBackground, true);
+
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+
+void Widget::on_btn_exit_clicked()
+{
+    qApp->quit();
 }
 
--- chapter7-frameless/widget.h
+++ chapter7-frameless/widget.h
@@ -15,6 +15,9 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_btn_exit_clicked();
+
 private:
     Ui::Widget *ui;
 };
