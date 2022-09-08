d45c30414658e0fdb0c7d17a5b316b859878137c
azhong <zczwzw@163.com>
2022-09-08 21:51:13 +0800

    chapter8-vertical-layout

---
+++ chapter8-vertical-layout/widget.cpp
@@ -0,0 +1,27 @@
+#include "widget.h"
+//#include "./ui_widget.h"
+#include <QtWidgets>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    auto *vbox = new QVBoxLayout(this);
+    auto *settings = new QPushButton("设置", this);
+    auto *accounts = new QPushButton("账户", this);
+    auto *loans = new QPushButton("存款", this);
+    auto *cash = new QPushButton("取款", this);
+
+    settings->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    accounts->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    loans->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    cash->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    vbox->addWidget(settings);
+    vbox->addWidget(accounts);
+    vbox->addWidget(loans);
+    vbox->addWidget(cash);
+}
+
+Widget::~Widget()
+{
+}
